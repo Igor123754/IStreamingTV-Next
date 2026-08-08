14a9010115f54bad6144403297e519f2e9c10891
Igor123754 <igormiladinovic3@gmail.com>
2026-08-08 18:54:04 +0200

    Refactor movie selection state management

--- app/src/main/java/com/igor/istreamingtv/MainActivity.kt
+++ app/src/main/java/com/igor/istreamingtv/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import com.igor.istreamingtv.data.remote.TmdbMovie
 import com.igor.istreamingtv.ui.details.MovieDetailsScreen
@@ -14,52 +13,29 @@ import com.igor.istreamingtv.ui.theme.IStreamingTheme
 
 class MainActivity : ComponentActivity() {
 
-    override fun onCreate(
-        savedInstanceState: Bundle?
-    ) {
+    private var selectedMovie by mutableStateOf<TmdbMovie?>(null)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
 
             IStreamingTheme {
 
-                var selectedMovie by rememberSaveable(
-                    stateSaver = androidx.compose.runtime.saveable.Saver(
-                        save = {
-                            listOf(
-                                it.id,
-                                it.displayTitle,
-                                it.displayDate,
-                                it.posterPath,
-                                it.backdropPath
-                            )
-                        },
-                        restore = {
-                            TmdbMovie(
-                                id = it[0] as Int,
-                                posterPath = it[3] as String?,
-                                backdropPath = it[4] as String?,
-                                displayTitle = it[1] as String,
-                                displayDate = it[2] as String
-                            )
-                        }
-                    )
-                ) {
-                    mutableStateOf<TmdbMovie?>(null)
-                }
+                val movie = selectedMovie
 
-                if (selectedMovie == null) {
+                if (movie == null) {
 
                     HomeScreen(
-                        onMovieClick = { movie ->
-                            selectedMovie = movie
+                        onMovieClick = { selected ->
+                            selectedMovie = selected
                         }
                     )
 
                 } else {
 
                     MovieDetailsScreen(
-                        movie = selectedMovie!!,
+                        movie = movie,
                         onBack = {
                             selectedMovie = null
                         }
