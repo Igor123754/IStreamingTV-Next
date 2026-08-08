52da9a8a6fe7248fc6e132427cd0f409ab0be733
Igor123754 <igormiladinovic3@gmail.com>
2026-08-08 06:49:20 +0200

    Update build.gradle.kts with defaultConfig and dependencies
    Added default configuration for application ID, SDK versions, and build config field for TMDB API key.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,24 @@ plugins {
 
 android {
     namespace = "com.igor.istreamingtv"
+
     compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.igor.istreamingtv"
+
+        minSdk = 23
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "0.1.0"
+
+        buildConfigField(
+            "String",
+            "TMDB_API_KEY",
+            "\"${System.getenv("TMDB_API_KEY") ?: ""}\""
+        )
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -17,16 +34,9 @@ android {
         jvmTarget = "17"
     }
 
-    defaultConfig {
-        applicationId = "com.igor.istreamingtv"
-        minSdk = 23
-        targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
-    }
-
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 
     packaging {
@@ -37,17 +47,51 @@ android {
 }
 
 dependencies {
+
+    // Android
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    implementation(
+        "androidx.activity:activity-compose:1.10.1"
+    )
+
+    // Compose
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2025.02.00"
+        )
+    )
+
     implementation("androidx.compose.ui:ui")
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
-    implementation("io.coil-kt:coil-compose:2.7.0")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
+
+    // ViewModel
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7"
+    )
+
+    // Retrofit
+    implementation(
+        "com.squareup.retrofit2:retrofit:2.11.0"
+    )
+
+    implementation(
+        "com.squareup.retrofit2:converter-gson:2.11.0"
+    )
+
+    // Coil
+    implementation(
+        "io.coil-kt:coil-compose:2.7.0"
+    )
 }
