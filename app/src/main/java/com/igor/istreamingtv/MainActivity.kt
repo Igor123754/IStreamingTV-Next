5d9d184d12263b0e83759f4c3291ccfe92ebc84b
Igor123754 <igormiladinovic3@gmail.com>
2026-08-08 05:41:54 +0200

    Create MainActivity with basic UI setup
    Initial implementation of MainActivity with a black background and centered text.

---
+++ app/src/main/java/com/igor/istreamingtv/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.igor.istreamingtv
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MaterialTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "IStreamingTV",
+                        color = Color.White
+                    )
+                }
+            }
+        }
+    }
+}
