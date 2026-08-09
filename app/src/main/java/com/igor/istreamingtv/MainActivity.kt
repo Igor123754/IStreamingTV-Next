49415ba24a06cf37a02f1f5c01560bde0d160e86
Igor123754 <igormiladinovic3@gmail.com>
2026-08-09 22:20:16 +0200

    Refactor MainActivity to use selectedMovieId

--- app/src/main/java/com/igor/istreamingtv/MainActivity.kt
+++ app/src/main/java/com/igor/istreamingtv/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import com.igor.istreamingtv.data.remote.TmdbMovie
 import com.igor.istreamingtv.ui.details.MovieDetailsScreen
 import com.igor.istreamingtv.ui.home.HomeScreen
 import com.igor.istreamingtv.ui.movies.MoviesScreen
@@ -15,82 +15,50 @@ import com.igor.istreamingtv.ui.theme.IStreamingTheme
 class MainActivity : ComponentActivity() {
 
     private var currentScreen by mutableStateOf(Screen.HOME)
-
-    /*
-     * Ekran sa kojeg smo otvorili Details.
-     *
-     * Ovo rešava problem:
-     * Home -> Details -> Nazad mora vratiti Home
-     * Movies -> Details -> Nazad mora vratiti Movies
-     */
     private var detailsReturnScreen by mutableStateOf(Screen.HOME)
-
-    private var selectedMovie by mutableStateOf<TmdbMovie?>(null)
+    private var selectedMovieId by mutableIntStateOf(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-
             IStreamingTheme {
-
                 when (currentScreen) {
-
                     Screen.HOME -> {
-
                         HomeScreen(
                             onMovieClick = { movie ->
-
-                                selectedMovie = movie
-
+                                selectedMovieId = movie.id
                                 detailsReturnScreen = Screen.HOME
-
                                 currentScreen = Screen.DETAILS
                             },
-
                             onMoviesClick = {
-
                                 currentScreen = Screen.MOVIES
                             }
                         )
                     }
 
                     Screen.MOVIES -> {
-
                         MoviesScreen(
                             onMovieClick = { movie ->
-
-                                selectedMovie = movie
-
+                                selectedMovieId = movie.id
                                 detailsReturnScreen = Screen.MOVIES
-
                                 currentScreen = Screen.DETAILS
                             },
-
                             onBack = {
-
                                 currentScreen = Screen.HOME
                             }
                         )
                     }
 
                     Screen.DETAILS -> {
-
-                        val movie = selectedMovie
-
-                        if (movie != null) {
-
+                        if (selectedMovieId != 0) {
                             MovieDetailsScreen(
-                                movie = movie,
-
+                                movieId = selectedMovieId,
                                 onBack = {
-
                                     currentScreen = detailsReturnScreen
                                 }
                             )
-
                         } else {
-
                             currentScreen = Screen.HOME
                         }
                     }
@@ -101,39 +69,15 @@ class MainActivity : ComponentActivity() {
 
     @Deprecated("Use OnBackPressedDispatcher instead")
     override fun onBackPressed() {
-
         when (currentScreen) {
-
-            Screen.DETAILS -> {
-
-                currentScreen = detailsReturnScreen
-            }
-
-            Screen.MOVIES -> {
-
-                currentScreen = Screen.HOME
-            }
-
-            Screen.HOME -> {
-
-                super.onBackPressed()
-            }
+            Screen.DETAILS -> currentScreen = detailsReturnScreen
+            Screen.MOVIES -> currentScreen = Screen.HOME
+            Screen.HOME -> super.onBackPressed()
         }
     }
 }
 
-/*
- * Svi ekrani aplikacije.
- *
- * Kasnije ćemo ovde dodati:
- *
- * SERIES
- * SEARCH
- * LIVE_TV
- * SETTINGS
- */
 private enum class Screen {
-
     HOME,
     MOVIES,
     DETAILS
