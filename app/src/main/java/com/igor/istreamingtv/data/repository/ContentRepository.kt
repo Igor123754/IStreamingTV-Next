992dcf96d621960c557a6959360da99fd4efd22a
Igor123754 <igormiladinovic3@gmail.com>
2026-08-10 04:05:37 +0200

    Refactor ContentRepository to use access token

--- app/src/main/java/com/igor/istreamingtv/data/repository/ContentRepository.kt
+++ app/src/main/java/com/igor/istreamingtv/data/repository/ContentRepository.kt
@@ -6,28 +6,27 @@ import com.igor.istreamingtv.data.remote.TmdbMovie
 import com.igor.istreamingtv.data.remote.TmdbMovieDetails
 
 class ContentRepository(
-    private val apiKey: String
+    private val accessToken: String
 ) {
+    private val api = TmdbClient.createRetrofit(accessToken).create(TmdbApi::class.java)
 
-    private val api = TmdbClient.retrofit.create(TmdbApi::class.java)
-
-    suspend fun getPopularMovies(page: Int = 1): List<TmdbMovie> {
-        return api.getPopularMovies(page, apiKey).results
+    suspend fun getPopularMovies(): List<TmdbMovie> {
+        return api.getPopularMovies().results
     }
 
-    suspend fun getTopRatedMovies(page: Int = 1): List<TmdbMovie> {
-        return api.getTopRatedMovies(page, apiKey).results
+    suspend fun getTopRatedMovies(): List<TmdbMovie> {
+        return api.getTopRatedMovies().results
     }
 
     suspend fun getTrendingMovies(): List<TmdbMovie> {
-        return api.getTrendingMovies(apiKey).results
+        return api.getTrendingMovies().results
     }
 
     suspend fun getTrendingSeries(): List<TmdbMovie> {
-        return api.getTrendingSeries(apiKey).results
+        return api.getTrendingSeries().results
     }
 
     suspend fun getMovieDetails(movieId: Int): TmdbMovieDetails {
-        return api.getMovieDetails(movieId, apiKey)
+        return api.getMovieDetails(movieId)
     }
 }
