f31607e753ed842daecc7e0bef0732719c15369b
Igor123754 <igormiladinovic3@gmail.com>
2026-08-10 04:04:33 +0200

    Simplify TmdbApi by removing query parameters
    Removed unnecessary API key query parameters from movie-related endpoints.

--- app/src/main/java/com/igor/istreamingtv/data/remote/TmdbApi.kt
+++ app/src/main/java/com/igor/istreamingtv/data/remote/TmdbApi.kt
@@ -2,35 +2,23 @@ package com.igor.istreamingtv.data.remote
 
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface TmdbApi {
 
     @GET("movie/popular")
-    suspend fun getPopularMovies(
-        @Query("page") page: Int = 1,
-        @Query("api_key") apiKey: String
-    ): MovieResponse
+    suspend fun getPopularMovies(): MovieResponse
 
     @GET("movie/top_rated")
-    suspend fun getTopRatedMovies(
-        @Query("page") page: Int = 1,
-        @Query("api_key") apiKey: String
-    ): MovieResponse
+    suspend fun getTopRatedMovies(): MovieResponse
 
     @GET("trending/movie/week")
-    suspend fun getTrendingMovies(
-        @Query("api_key") apiKey: String
-    ): MovieResponse
+    suspend fun getTrendingMovies(): MovieResponse
 
     @GET("trending/tv/week")
-    suspend fun getTrendingSeries(
-        @Query("api_key") apiKey: String
-    ): MovieResponse
+    suspend fun getTrendingSeries(): MovieResponse
 
     @GET("movie/{movie_id}")
     suspend fun getMovieDetails(
-        @Path("movie_id") movieId: Int,
-        @Query("api_key") apiKey: String
+        @Path("movie_id") movieId: Int
     ): TmdbMovieDetails
 }
