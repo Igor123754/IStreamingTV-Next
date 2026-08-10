0b26055212db4b4ff747c9b957f5c850d32fad42
Igor123754 <igormiladinovic3@gmail.com>
2026-08-10 04:03:23 +0200

    Refactor TmdbClient to use dynamic Retrofit creation
    Refactor TmdbClient to create Retrofit instance with access token and OkHttp client.

--- app/src/main/java/com/igor/istreamingtv/data/remote/TmdbClient.kt
+++ app/src/main/java/com/igor/istreamingtv/data/remote/TmdbClient.kt
@@ -1,12 +1,26 @@
 package com.igor.istreamingtv.data.remote
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object TmdbClient {
 
-    val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl("https://api.themoviedb.org/3/")
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    fun createRetrofit(accessToken: String): Retrofit {
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $accessToken")
+                    .addHeader("accept", "application/json")
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl("https://api.themoviedb.org/3/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
