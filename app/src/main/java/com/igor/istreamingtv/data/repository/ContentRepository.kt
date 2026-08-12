9abd8bee79b6ad6227bd7216c719f7cefc8812f1
Igor123754 <igormiladinovic3@gmail.com>
2026-08-12 04:51:52 +0200

    Refactor repository methods to use single-expression syntax

--- app/src/main/java/com/igor/istreamingtv/data/repository/ContentRepository.kt
+++ app/src/main/java/com/igor/istreamingtv/data/repository/ContentRepository.kt
@@ -2,6 +2,7 @@ package com.igor.istreamingtv.data.repository
 
 import com.igor.istreamingtv.data.remote.TmdbApi
 import com.igor.istreamingtv.data.remote.TmdbClient
+import com.igor.istreamingtv.data.remote.TmdbHeroDetails
 import com.igor.istreamingtv.data.remote.TmdbMovie
 import com.igor.istreamingtv.data.remote.TmdbMovieDetails
 
@@ -10,23 +11,19 @@ class ContentRepository(
 ) {
     private val api = TmdbClient.createRetrofit(accessToken).create(TmdbApi::class.java)
 
-    suspend fun getPopularMovies(): List<TmdbMovie> {
-        return api.getPopularMovies().results
-    }
+    suspend fun getPopularMovies(): List<TmdbMovie> = api.getPopularMovies().results
 
-    suspend fun getTopRatedMovies(): List<TmdbMovie> {
-        return api.getTopRatedMovies().results
-    }
+    suspend fun getTopRatedMovies(): List<TmdbMovie> = api.getTopRatedMovies().results
 
-    suspend fun getTrendingMovies(): List<TmdbMovie> {
-        return api.getTrendingMovies().results
-    }
+    suspend fun getTrendingMovies(): List<TmdbMovie> = api.getTrendingMovies().results
 
-    suspend fun getTrendingSeries(): List<TmdbMovie> {
-        return api.getTrendingSeries().results
-    }
+    suspend fun getTrendingSeries(): List<TmdbMovie> = api.getTrendingSeries().results
 
-    suspend fun getMovieDetails(movieId: Int): TmdbMovieDetails {
-        return api.getMovieDetails(movieId)
-    }
+    suspend fun getMovieDetails(movieId: Int): TmdbMovieDetails = api.getMovieDetails(movieId)
+
+    suspend fun getMovieHeroDetails(movieId: Int): TmdbHeroDetails =
+        api.getMovieHeroDetails(movieId)
+
+    suspend fun getTvHeroDetails(tvId: Int): TmdbHeroDetails =
+        api.getTvHeroDetails(tvId)
 }
