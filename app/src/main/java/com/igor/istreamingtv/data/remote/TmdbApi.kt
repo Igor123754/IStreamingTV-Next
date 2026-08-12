9913c6b9c9b1c16e2dde6b672d5e7384d77db23c
Igor123754 <igormiladinovic3@gmail.com>
2026-08-12 05:57:32 +0200

    Add language parameter to TmdbApi endpoints

--- app/src/main/java/com/igor/istreamingtv/data/remote/TmdbApi.kt
+++ app/src/main/java/com/igor/istreamingtv/data/remote/TmdbApi.kt
@@ -6,22 +6,23 @@ import retrofit2.http.Query
 
 interface TmdbApi {
 
-    @GET("movie/popular")
+    // Kataloge vučemo sa language=sr-RS → naslovi/opisi na srpskom kad postoje
+    @GET("movie/popular?language=sr-RS")
     suspend fun getPopularMovies(): MovieResponse
 
-    @GET("movie/top_rated")
+    @GET("movie/top_rated?language=sr-RS")
     suspend fun getTopRatedMovies(): MovieResponse
 
-    @GET("trending/movie/week")
+    @GET("trending/movie/week?language=sr-RS")
     suspend fun getTrendingMovies(): MovieResponse
 
-    @GET("trending/tv/week")
+    @GET("trending/tv/week?language=sr-RS")
     suspend fun getTrendingSeries(): MovieResponse
 
-    @GET("tv/popular")
+    @GET("tv/popular?language=sr-RS")
     suspend fun getPopularSeries(): MovieResponse
 
-    @GET("tv/top_rated")
+    @GET("tv/top_rated?language=sr-RS")
     suspend fun getTopRatedSeries(): MovieResponse
 
     @GET("movie/{movie_id}")
